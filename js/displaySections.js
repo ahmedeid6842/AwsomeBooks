da76d41db3f827c4c04a557f6d14fd894fdae168
Ruthmy <Ruryemo.exe@gmail.com>
2023-05-30 15:35:29 -0600

    Added the Single Page App functionality

--- js/displaySections.js
+++ js/displaySections.js
@@ -5,3 +5,20 @@ const listBook = document.querySelector('.book-list');
 const addBook = document.querySelector('.add-section');
 const contactSection = document.querySelector('.contact-section');
 
+navItemList.addEventListener('click', () => {
+  addBook.classList.add('hidden');
+  contactSection.classList.add('hidden');
+  listBook.classList.remove('hidden');
+});
+
+navItemAdd.addEventListener('click', () => {
+  contactSection.classList.add('hidden');
+  listBook.classList.add('hidden');
+  addBook.classList.remove('hidden');
+});
+
+navItemContact.addEventListener('click', () => {
+  addBook.classList.add('hidden');
+  listBook.classList.add('hidden');
+  contactSection.classList.remove('hidden');
+});
