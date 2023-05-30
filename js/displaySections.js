7e257b0d418aea18aec51e25569653f55cd3a367
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-05-31 00:21:53 +0300

    Added html for the sections

---
+++ js/displaySections.js
@@ -0,0 +1,7 @@
+const navItemList = document.querySelector('.nav-item.list');
+const navItemAdd = document.querySelector('.nav-item.add-new');
+const navItemContact = document.querySelector('.nav-item.contact');
+const listBook = document.querySelector('.book-list');
+const addBook = document.querySelector('.add-section');
+const contactSection = document.querySelector('.contact-section');
+
